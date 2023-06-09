07b2a71571dfc89226c993d4233e955666f3abd5
Igi-it <igi701580@gmail.com>
2023-06-09 09:04:50 +0200

    02

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('goldline.html', views.goldline, name='goldline'),
+    path('automag.html', views.automag, name='automag'),
+    path('m249.html', views.m249, name='m249'),
+    path('beretta.html', views.beretta, name='beretta'),
+
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def goldline(request):
+    return render(request, 'goldline.html')
+
+def m249(request):
+    return render(request, 'm249.html')
+
+def automag(request):
+    return render(request, 'automag.html')
+
+def beretta(request):
+    return render(request, 'beretta.html')
